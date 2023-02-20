5b527cb1b1f04b60d364f3f48f7b6eac4f3449b3
Guilherme F. Videira <123119430+GuilhermeVideira@users.noreply.github.com>
2023-02-20 19:02:56 -0300

    Add files via upload

---
+++ Conversão de graus/Conversão de graus/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversão_de_graus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Tcel, Tkel, Ifah;
+            Tcel = int.Parse(textBox1.Text);
+
+            Tkel = Tcel + 273;
+            Ifah = (((Tcel * 9) / 5) + 32);
+
+            label3.Text = Tkel.ToString();
+            label5.Text = Ifah.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
