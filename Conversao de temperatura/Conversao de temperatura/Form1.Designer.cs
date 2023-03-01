e54e3434891f537d6ca0816bfd2b3679c169f837
Guilherme F. Videira <123119430+GuilhermeVideira@users.noreply.github.com>
2023-03-01 14:51:54 -0300

    Add files via upload

---
+++ Conversao de temperatura/Conversao de temperatura/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Conversao_de_temperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Celsius --- Farenheit",
+            "Celsius --- Kelvin ",
+            "Farenheit --- Celsius",
+            "Farenheit --- Kelvin",
+            "Kelvin --- Celsius ",
+            "Kelvin --- Farenheit"});
+            this.comboBox1.Location = new System.Drawing.Point(42, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(374, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(42, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(237, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(179, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 178);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Conversão de Temperatura";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Conversao de temperatura/Conversao de temperatura/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversao_de_temperatura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double Farenheit;
+        private double Celsius;
+        private double Kelvin;
+
+        private double FarenheitToCelsius
+        {
+            get { return (Farenheit - 32) * 5 / 9; }
+            set { Farenheit = value; }
+        }
+
+        private double FarenheitToKelvin
+        {
+            get { return (Farenheit - 32) * 5 / 9 + 273.15; }
+            set { Farenheit = value; }
+        }
+
+        private double CelsiusToKelvin
+        {
+            get { return Celsius + 273.15; }
+            set { Celsius = value; }
+        }
+
+        private double CelsiusToFarenheit
+        {
+            get { return (Celsius * 9 / 5) + 32; }
+            set { Celsius = value; }
+        }
+
+        private double KelvinToCelsius
+        {
+            get { return Kelvin - 273.15; }
+            set { Kelvin = value; }
+        }
+
+        private double KelvinToFarenheit
+        {
+            get { return (Kelvin - 273.15) * 9/5 + 32; }
+            set { Kelvin = value; }
+        }
+
+        private void Converter()
+        {
+            try { 
+            if (string.IsNullOrEmpty(textBox1.Text) || textBox1.Text == "-")
+            {
+                textBox2.Text = "";
+
+            } 
+            else if(comboBox1.Text == "Farenheit --- Celsius ")
+            {
+                FarenheitToCelsius = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = FarenheitToCelsius.ToString();
+            }
+            else if (comboBox1.Text == "Farenheit --- Kelvin ")
+            {
+                FarenheitToKelvin = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = FarenheitToKelvin.ToString();
+            }
+            else if (comboBox1.Text == "Celsius --- Farenheit")
+            {
+                CelsiusToFarenheit = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = CelsiusToFarenheit.ToString();
+            }
+            else if (comboBox1.Text == "Celsius --- Kelvin")
+            {
+                CelsiusToKelvin = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = CelsiusToKelvin.ToString();
+            }
+            else if (comboBox1.Text == "Kelvin --- Celsius")
+            {
+                KelvinToCelsius = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = KelvinToCelsius.ToString();
+            }
+            else if (comboBox1.Text == "Kelvin --- Farenheit")
+            {
+                KelvinToFarenheit = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = KelvinToFarenheit.ToString();
+            }
+          } catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if ((ch == 46 && textBox1.Text.IndexOf(" . ") != -1) ||
+            (ch == 45 && textBox1.Text.IndexOf(" - ") != -1) ||
+            (ch == 45 && textBox1.Text.IndexOf(" . ") != -1) ||
+
+            (!Char.IsDigit(ch) && ch != 8 && ch != 46 && ch != 45))
+
+            {
+                e.Handled = true;
+                return;
+            }
+
+        }
+
+        private void ChangeLabel1()
+        {
+            switch(comboBox1.Text)
+            {
+                case "Farenheit --- Celsius":
+                    label1.Text = "Farenheit";
+                    label2.Text = "Celsius";
+                    break;
+
+                case "Farenheit --- Kelvin":
+                    label1.Text = "Farenheit";
+                    label2.Text = "Kelvin";
+                    break;
+
+                case "Celsius --- Kelvin":
+                    label1.Text = "Celsius";
+                    label2.Text = "Kelvin";
+                    break;
+
+                case "Celsius --- Farenheit":
+                    label1.Text = "Celsius";
+                    label2.Text = "Farenheit";
+                    break;
+
+                case "Kelvin --- Farenheit":
+                    label1.Text = "Kelvin";
+                    label2.Text = "Farenheit";
+                    break;
+
+                case "Kelvin --- Celsius":
+                    label1.Text = "Kelvin";
+                    label2.Text = "Celsius";
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Converter();
+            ChangeLabel1();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Converter();
+        }
+    }
+}
